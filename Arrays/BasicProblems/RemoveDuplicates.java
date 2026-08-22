a6bdfc585ee49256ee74dc506b6b4c96c992e482
Faizan Yousuf <reshifaixxan980@gmail.com>
2026-08-22 13:42:09 +0530

    add removeduplicates

---
+++ Arrays/BasicProblems/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+public class RemoveDuplicates{
+    public static void main(String[] args){
+
+    }
+}
+class Solution {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = arr.length-1;i > 0; i--){
+            
+            if(arr[i] == arr[i-1]){
+                continue;
+            }else{
+                list.add(0,arr[i]);
+            }
+        }
+        
+        list.add(0,arr[0]);
+        return list;
+        
+     
+    }
+}
