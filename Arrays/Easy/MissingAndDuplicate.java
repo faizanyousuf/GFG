814e0593883c24ea0de3ee70c6200ae28bc1b070
Faizan Yousuf <reshifaixxan980@gmail.com>
2026-08-24 16:33:54 +0530

    added missing and duplicate appraoch one

---
+++ Arrays/Easy/MissingAndDuplicate.java
@@ -0,0 +1,33 @@
+public class MissingAndDuplicate{
+    public static void main(String[] args){
+
+
+    }
+}
+class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        // code here
+        
+        //approach 1;
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int n = arr.length;
+        //  boolean [] visited = new boolean[arr.length];
+        // int missing = -1;
+        // for(int i = 0; i < n; i++){
+        //     visited[arr[i] - 1] = true;
+        // }
+        
+        // for(int i = 0; i < n; i++){
+        //     if(visited[i] == false){
+        //         missing = i + 1;
+        //     }
+        // }
+        
+        // int duplicate = arr[missing];
+        
+        // list.add(duplicate);
+        // list.add(missing);
+           
+        //   return list;
+    }
+}
