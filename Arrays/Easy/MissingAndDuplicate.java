d68a4abd80a09017c8fea299035210fa70bcc948
Faizan Yousuf <reshifaixxan980@gmail.com>
2026-08-24 17:17:10 +0530

    changed missing and duplicate code

--- Arrays/Easy/MissingAndDuplicate.java
+++ Arrays/Easy/MissingAndDuplicate.java
@@ -9,24 +9,32 @@ class Solution {
         // code here
         
         //approach 1;
+class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        // code here
+        
+        //approach 1;
         // ArrayList<Integer> list = new ArrayList<>();
-        // int n = arr.length;
+        // long n = arr.length;
+        // long actualSum = n*(n +1)/2;
         //  boolean [] visited = new boolean[arr.length];
-        // int missing = -1;
+        // int index = -1;
+        // int sum = 0;
         // for(int i = 0; i < n; i++){
         //     visited[arr[i] - 1] = true;
+        //     sum += arr[i];
         // }
         
         // for(int i = 0; i < n; i++){
         //     if(visited[i] == false){
-        //         missing = i + 1;
+        //         index = i;
         //     }
         // }
+        // long missing = index + 1;
+        // long duplicate = ((sum + missing)-actualSum);
         
-        // int duplicate = arr[missing];
-        
-        // list.add(duplicate);
-        // list.add(missing);
+        // list.add((int)duplicate);
+        // list.add((int)missing);
            
         //   return list;
     }
